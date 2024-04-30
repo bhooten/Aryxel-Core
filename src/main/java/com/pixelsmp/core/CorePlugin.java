75d60a54742dc8f8274e9155d579ea81c94dd6f1
Bradley Hooten <bradleyah02@gmail.com>
2024-04-30 13:51:33 -0400

    Create initial plugin.yml and main class

---
+++ src/main/java/com/pixelsmp/core/CorePlugin.java
@@ -0,0 +1,16 @@
+package com.pixelsmp.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CorePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
