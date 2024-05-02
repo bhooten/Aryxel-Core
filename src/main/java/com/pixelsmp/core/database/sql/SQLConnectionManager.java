ad79159c5bff6245bef3dedef8df9c86c5d65a33
Bradley Hooten <bradleyah02@gmail.com>
2024-05-02 01:47:44 -0400

    Add support for asynchronous parameterized SQL query functions and updates, plus additional configuration options for HikariCP and the SQL worker pool size

--- src/main/java/com/pixelsmp/core/database/sql/SQLConnectionManager.java
+++ src/main/java/com/pixelsmp/core/database/sql/SQLConnectionManager.java
@@ -6,22 +6,17 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.bukkit.Bukkit;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SQLConnectionManager
 {
-    private final String jdbcUrl;
-    private final String hostname;
-    private final String username;
-    private final String password;
-    private final String database;
-    private final int port;
     private final ExecutorService executorService;
-    private volatile Connection connection;
-    private HikariConfig hikariConfig = new HikariConfig();
     private HikariDataSource dataSource;
 
     /**
@@ -37,21 +32,18 @@ public class SQLConnectionManager
      * @param database Name of the database to connect to
      * @param port Port of the MySQL server
      */
-    public SQLConnectionManager(String hostname, String username, String password, String database, int port) {
-        this.hostname = hostname;
-        this.username = username;
-        this.password = password;
-        this.database = database;
-        this.port = port;
-        this.jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + database;
-        this.executorService = Executors.newSingleThreadExecutor();
+    public SQLConnectionManager(String hostname, String username, String password, String database, int port,
+                                int hikariMaximumPoolSize, long hikariTimeoutMillis, int threadPoolSize) {
+        String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + database;
+        executorService = Executors.newFixedThreadPool(threadPoolSize);
 
         // Set proper parameters on the HikariCP configuration
+        HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(jdbcUrl);
         hikariConfig.setUsername(username);
         hikariConfig.setPassword(password);
-        hikariConfig.setMaximumPoolSize(10);
-        hikariConfig.setConnectionTimeout(3000);
+        hikariConfig.setMaximumPoolSize(hikariMaximumPoolSize);
+        hikariConfig.setConnectionTimeout(hikariTimeoutMillis);
 
         try
         {
@@ -111,10 +103,10 @@ public class SQLConnectionManager
      *
      * @since 1.0.0
      *
-     * @return Optional-wrapped Connection to the database
+     * @return Connection to the database
      */
-    public Optional<HikariDataSource> getDataSource() {
-        return Optional.ofNullable(dataSource);
+    public HikariDataSource getDataSource() {
+        return dataSource;
     }
 
     /**
@@ -130,5 +122,98 @@ public class SQLConnectionManager
             dataSource.close();
             Bukkit.getLogger().info(ChatFormatter.formatChatMessage("Database Core", "Successfully closed HikariCP datasource.", false));
         }
+    }
+
+    /**
+     * Executes a query on the database asynchronously and returns a CompletableFuture containing the result.
+     *
+     * @author Bradley Hooten (bradleyah02@gmail.com)
+     *
+     * @since 1.0.0
+     *
+     * @param query The MySQL query to execute
+     * @param handler The handler function to process the ResultSet
+     * @return CompletableFuture containing the result of the query
+     * @param <T> The type of the result
+     */
+    public <T> CompletableFuture<T> executeQueryAsync(String query, ResultSetHandlerFunction<T> handler,
+                                                      Object... parameters) {
+        return CompletableFuture.supplyAsync(() -> {
+            // Try-with-resources block to handle the connection, statement, and result set
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query))
+            {
+                // Set the parameters for the query
+                for(int i = 0; i < parameters.length; i++)
+                {
+                    statement.setObject(i + 1, parameters[i]);
+                }
+
+                try(ResultSet resultSet = statement.executeQuery())
+                {
+                    // Call the handler function with the ResultSet
+                    return handler.apply(resultSet);
+                }
+            }
+            catch (SQLException e)
+            {
+                // Pass exception up the call stack to be handled by the caller
+                throw new CompletionException(e);
+            }
+        }, executorService);
+    }
+
+    /**
+     * Executes an update query on the database asynchronously.
+     *
+     * @author Bradley Hooten (bradleyah02@gmail.com)
+     *
+     * @since 1.0.0
+     *
+     * @param query The MySQL statement to execute
+     *
+     * @return CompletableFuture containing the result of the query; -1 = error, 0 = no rows affected, >0 = rows affected
+     */
+    public CompletableFuture<Integer> executeUpdateAsync(String query, Object... parameters) {
+        return CompletableFuture.supplyAsync(() -> {
+            // Initialize the result to -1 to fail-first
+            int result = -1;
+
+            // Try-with-resources block to handle the connection and statement
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query))
+            {
+                // Set the parameters for the query
+                for(int i = 0; i < parameters.length; i++)
+                {
+                    statement.setObject(i + 1, parameters[i]);
+                }
+
+                // Execute the query and store the result
+                result = statement.executeUpdate();
+            }
+            catch (SQLException e)
+            {
+                // Pass exception up the call stack to be handled by the caller
+                throw new CompletionException(e);
+            }
+
+            // Return the result of the query
+            return result;
+        }, executorService);
+    }
+
+    /**
+     * This functional interface is used to handle the ResultSet from a query.
+     *
+     * @author Bradley Hooten (bradleyah02@gmail.com)
+     *
+     * @since 1.0.0
+     *
+     * @param <R> The type of the result
+     */
+    public interface ResultSetHandlerFunction<R>
+    {
+        R apply(ResultSet rs) throws SQLException;
     }
 }
